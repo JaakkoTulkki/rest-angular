f47f9819fa69ce32feddd008943031b49e115f92
Jaakko Tulkki <jaakko.tulkki@gmail.com>
2015-03-21 23:26:22 +0100

    added

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,16 +19,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'static/bower_components/jquery/dist/jquery.js',
-      'static/bower_components/angular/angular.js',
-      'static/bower_components/bootstrap/dist/js/bootstrap.js',
-      'static/bower_components/angular-animate/angular-animate.js',
-      'static/bower_components/angular-cookies/angular-cookies.js',
-      'static/bower_components/angular-resource/angular-resource.js',
-      'static/bower_components/angular-route/angular-route.js',
-      'static/bower_components/angular-sanitize/angular-sanitize.js',
-      'static/bower_components/angular-touch/angular-touch.js',
-      'static/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
